b35ab919f02c1297dcfdd0adb3cef0062aee1b56
newton101 <miguil@hotmail.co.uk>
2013-04-15 15:07:51 +0100

    create story model and controller

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,5 @@
+class StoriesController < ApplicationController
+
+
+
+end
---
+++ db/migrate/20130415134958_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
