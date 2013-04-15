73ad008fa727c3f26bbce45ce4fcafc2b26af00a
newton101 <miguil@hotmail.co.uk>
2013-04-15 13:45:29 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryTree::Application.config.secret_token = 'dd53415aeb1da5d23e952c1a88391081ac2d1a5e20532f3322242dd41432f858709732c136c8a05e4bd25205f17f7dd245d386242702a23fcdab1a69d6519f2b'
