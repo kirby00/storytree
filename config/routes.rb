a2c711c2ab97fc7a61b52300b8a49aba914933a9
kirby00 <mbkirby@gmail.com>
2013-04-19 10:57:22 +0100

    add facebook like

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,11 @@ resources :additions do
   end
 end
 
-
-
 resources :stories
 
 match 'auth/:provider/callback', to: 'sessions#create'
 match 'auth/failure', to: redirect('/')
 match 'signout', to: 'sessions#destroy', as: 'signout'
-
-root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
