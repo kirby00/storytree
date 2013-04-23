87e82d62cf59fb5c94a1cdc1de8a2a5ca2c45b39
robb <rob_boyland@hotmail.com>
2013-04-23 14:50:21 +0100

    add css styling/layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,6 @@
 
 $(document).ready(function() {
 
-
   $('#new_addition').submit(function(e) {
 
     $.ajax({
@@ -83,4 +82,18 @@ $(document).ready(function() {
     after: function(){}     // Function: After callback
   });
 
+  $('#tabs li a:not(:first)').addClass('inactive');
+  $('.container').hide();
+  $('.container:first').show();
+
+  $('#tabs li a').click(function(){
+    var t = $(this).attr('id');
+  if($(this).hasClass('inactive')){ //this is the start of our condition
+    $('#tabs li a').addClass('inactive');
+    $(this).removeClass('inactive');
+
+    $('.container').hide();
+    $('#'+ t + 'C').fadeIn('slow');
+ }
+});
 });
