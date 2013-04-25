3c14c87a3b43c1db295f729b515c136e01a01682
kirby00 <mbkirby@gmail.com>
2013-04-25 11:05:20 +0100

    revert to previous additions#create

--- app/controllers/additions_controller.rb
+++ app/controllers/additions_controller.rb
@@ -5,26 +5,27 @@ class AdditionsController < ApplicationController
   end
 
   def create
-      addition = params[:addition]
-      addition[:story_id] = addition[:story_id].to_i
-      round = Round.find_all_by_story_id(addition[:story_id]).last
+    addition = (params.slice("story_id"))
+    addition[:content] = params[:addition][:content]
+    addition[:story_id] = addition[:story_id].to_i
+    round = Round.find_all_by_story_id(addition[:story_id]).last
 
 
-      if round.winner_id != nil
-        new_round = Round.create({:story_id => addition[:story_id]})
-        @addition = Addition.create({round_id: new_round.id, user_id: current_user.id, content: addition[:content]})
-      else
-        @addition = Addition.create({round_id: round.id, user_id: current_user.id, content: addition[:content]})
-      end
+    if round.winner_id != nil
+      new_round = Round.create({:story_id => addition[:story_id]})
+      @addition = Addition.create({round_id: new_round.id, user_id: current_user.id, content: addition[:content]})
+    else
+      @addition = Addition.create({round_id: round.id, user_id: current_user.id, content: addition[:content]})
+    end
 
-      if @addition.save
-        render :json => { :status => 'true',
-                          :content => @addition.content,
-                          :addition_id => @addition.id
-                        }
-      else
-        render :json => { :status => 'false' }
-      end
+    if @addition.save
+      render :json => { :status => 'true',
+                        :content => @addition.content,
+                        :addition_id => @addition.id
+                      }
+    else
+      render :json => { :status => 'false' }
+    end
   end
 
     def vote_true
